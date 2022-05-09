9cc0a36450a32e428aabbff1a42c0c5a7a3d9384
gregaswright <greg.a.s.wright@gmail.com>
2022-05-09 11:51:06 -0700

    needs to be cleaned up but its working

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(userRouter)
 app.use(cityRouter)
 app.use(weatherRouter)
